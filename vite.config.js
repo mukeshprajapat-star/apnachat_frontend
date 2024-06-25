c6a6761ece70f817fb11a7628cbb3ab1dc1b338b
mukeshprajapat-star <mukeshprajapat7529@gmail.com>
2024-06-25 10:00:39 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:3000/',
+        target: 'https://apnachat-backend.onrender.com/',
         changeOrigin: true,
       },
     },
